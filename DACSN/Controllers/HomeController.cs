4be128b81be926243763bcbcdc522e63084f3866
HaQuocPhong <phongha676@gmail.com>
2021-09-12 09:06:12 +0700

    update

---
+++ DACSN/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DACSN.Models;
+
+namespace DACSN.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbDACSNDataContext data = new dbDACSNDataContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TinTuc()
+        {
+            return View();
+        }
+
+        public ActionResult DienBienDich()
+        {
+            return View();
+        }
+
+        public ActionResult Video()
+        {
+            return View();
+        }
+
+        public ActionResult KhuyenCao()
+        {
+            return View();
+        }
+
+        public ActionResult DieuCanBiet()
+        {
+            return View();
+        }
+
+
+    }
+}
